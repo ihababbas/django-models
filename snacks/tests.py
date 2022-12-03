d0b4e1f91e8df3ea18f54718bd4227121a6e2c7a
ihababbas <ihababbas26@gmail.com>
2022-12-03 17:43:44 +0200

    done

--- snacks/tests.py
+++ snacks/tests.py
@@ -12,5 +12,13 @@ class ThingsTests(TestCase):
         url = reverse('home')
         response = self.client.get(url)
         self.assertTemplateUsed(response,'home.html')
+    def test_snack_page_status(self):
+        url = reverse('snacks')
+        response = self.client.get(url)
+        print(response)
+        self.assertEqual(response.status_code, 200)
+    def test_snack_page_template(self):
+        url = reverse('snacks')
+        response = self.client.get(url)
+        self.assertTemplateUsed(response,'snack_list.html')
     
-
